deb80d57e97af17aed0372fa3edf153e65b73c7c
Mijung Lee <Mijung@Stephens-MacBook-Air.local>
2017-10-10 10:55:50 -0700

    added test to check question functionality

--- question.rb
+++ question.rb
@@ -2,13 +2,13 @@ module MathGame
   class Question
 
     def initialize
-      @num1 = Random.rand(20)
-      @num2 = Random.rand(20)
+      @num1 = Random.rand(1..20)
+      @num2 = Random.rand(1..20)
       @answer = @num1 + @num2
     end
     
     def question
-      "What does #{num1} plus #{num2} equal?"
+      "What does #{@num1} plus #{@num2} equal?"
     end
 
     def correct?(answer)
@@ -16,4 +16,10 @@ module MathGame
     end
 
   end
+
+  # test
+  q = Question.new
+  puts q.question
+  a = 8
+  puts q.correct?(a)
 end
